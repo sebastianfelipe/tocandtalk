763299acf00a6af6b961c0619e104450c1c89770
Sebastián Felipe Torres Garfe <sftorres@alumnos.utfsm.cl>
2015-11-18 18:20:26 +0100

    Variable names changes in the login view

--- views/desktop/login/js/error_handling.js
+++ views/desktop/login/js/error_handling.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     // Nombre de usuario
 
-    if (error_handler.indexOf("error_user;") > -1) {
+    if (errors.indexOf("error_user;") > -1) {
         $("#error_user").show();
     }
 });
