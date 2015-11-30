1f80d9bc7a2a23665ae9f83c12b3ad8667b39242
Sebastián Felipe <sebastian.torres@alumnos.usm.cl>
2015-11-30 15:01:30 -0300

    Uso del primer nombre del usuario en las vistas de home, friends y profile. Envio de lenguajes disponibles a todas las vistas. Falta enviar la informacion de la descripcion y el resto de la info a la de profile y actualizar la lista de lenguajes de la busqueda. Mas adelante podremos colocar los lenguajes de interes primero quiza en la de home. Por ahora enfoquemonos en el envio de la informacion.

--- routes/friends.js
+++ routes/friends.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
+
+var db = require('../database/configuration.js');
+
+var schema = require('../database/schema.js');
+var models = require('../database/models.js');
 
 // Module Imports
 var authenticate_module = require('../modules/authenticate.js');
 var authenticate = authenticate_module.authenticate;
 
 router.get('/', authenticate, function (req, res) {
-	return res.render('friends/index.html', {forceType: "desktop", errors: ""});
-});
+  async.parallel({
+      languages: function(callback){
+          setTimeout(function(){
+              models.Language.find().sort([['name', 1]]).exec(function (err, docs) {
+                callback(err, docs);
+              })
+          }, 200);
+      },
+      user: function(callback) {
+          setTimeout(function(){
+              models.User.findOne({_username: req.session.username}, {password: 0}).exec(function (err, doc) {
+                callback(err, doc);
+              })
+          }, 200);
+      }
+  },
+  function(err, results) {
+  	 return res.render('friends/index.html', {forceType: "desktop", user: results.user, languages: results.languages, req: req.body, errors: ""});
+  });});
 
 module.exports = router;
