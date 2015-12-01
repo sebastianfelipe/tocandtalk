715826bd6cabb40aabdc9284c677a95512369c48
Sebastián Felipe Torres Garfe <sftorres@alumnos.utfsm.cl>
2015-12-01 15:10:46 +0100

    modificación autenticación para controlador de registro

--- modules/authenticate.js
+++ modules/authenticate.js
@@ -1,13 +1,27 @@
 var authenticate = function (req, res, next)
 {
-  if (!req.session.username)
-  {
-    return res.render('login/index.html', {forceType: "desktop", errors: ""}); }
-  
-  else
-  {
-    return next();
-  }
+	if (!req.session.username)
+	{
+		return res.redirect('/login');
+		//return res.render('login/index.html', {forceType: "desktop", errors: ""});
+	}
+	else
+	{
+		return next();
+	}
+}
+
+var authenticateRegister = function (req, res, next)
+{
+	if (req.session.username)
+	{
+		return res.redirect('/');
+	}
+	else
+	{
+		return next();
+	}
 }
 
 module.exports.authenticate = authenticate;
+module.exports.authenticateRegister = authenticateRegister;
