022aed51be57ceda1415845a498a8d3f1aae526c
Sebastián Felipe <sebastian.torres@alumnos.usm.cl>
2015-12-25 18:07:10 -0300

    traying to test https with ws and wss problems

--- routes/talk.js
+++ routes/talk.js
@@ -7,7 +7,13 @@ var authenticate_module = require('../modules/authenticate.js');
 var authenticate = authenticate_module.authenticate;
 
 router.get('/', authenticate, function (req, res) {
-  return res.render('talk2/index.html', {forceType: "desktop", username: req.session.username, server_ip: ip.address(), protocol: req.protocol });
+	var protocol = req.protocol;
+	console.log(req.secure);
+	if (req.secure)
+	{
+		protocol += "s";
+	}
+  return res.render('talk2/index.html', {forceType: "desktop", username: req.session.username, server_ip: ip.address(), protocol: protocol });
 });
 
 module.exports = router;
