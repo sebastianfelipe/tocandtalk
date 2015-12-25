b444017d99d1a12e0efca7409982dc19044a6db9
Sebastián Felipe <sebastian.torres@alumnos.usm.cl>
2015-12-25 01:29:09 -0300

    traying to test https with ws and wss problems

--- views/shared/js/main.js
+++ views/shared/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
   if (!refs.socket)
   {
-    var url = "http://https.tocandtalk.com/"
+    var url = "ws://https.tocandtalk.com/"
     refs.socket = io(url);
     console.log(refs.socket);
     refs.socket.on('receiveConnection', function(data) {
