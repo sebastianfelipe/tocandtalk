569ae99e4a48da91681d83e4ad6c7d8fbb6303e9
Kaneki- <fabian.dasilva.12@sansano.usm.cl>
2016-02-16 12:42:54 -0300

    Profile Updated

--- views/desktop/profile/assets/js/frontend/user-image.js
+++ views/desktop/profile/assets/js/frontend/user-image.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-	$('.usr-image').css("background-image", "url(desktop/shared/img/user_image_example.jpg)");
+	$('.usr-image').css("background-image", "url(desktop/shared/assets/img/user-image-example.jpg)");
 
     $('#usr-image-background').backgroundBlur({
-	    imageURL : "desktop/shared/img/user_image_example.jpg",
+	    imageURL : "desktop/shared/assets/img/user-image-example.jpg",
 	    blurAmount : 30,
 	    imageClass : 'tinted-bg-blur'
 	});
