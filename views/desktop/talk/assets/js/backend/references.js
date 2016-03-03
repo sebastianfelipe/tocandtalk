81f5e50ec32b5e20619912dcf0d9dc7abedf806e
Sebastián Felipe <sebastian.torres.garfe@gmail.com>
2016-03-03 17:42:21 -0300

    db update

--- views/desktop/talk/assets/js/backend/references.js
+++ views/desktop/talk/assets/js/backend/references.js
@@ -11,10 +11,10 @@ refs.sources = {};
 
 refs.meta.conn = {};
 refs.meta.conn.serverIp = null;
-refs.meta.conn.hostName = '192.168.1.109',//'https.tocandtalk.com';
+refs.meta.conn.hostName = 'https.tocandtalk.com';
 refs.meta.conn.serverPort = 4080;//443;
 refs.meta.conn.secure = false;
-refs.meta.conn.url = 'http://192.168.1.109:4080';
+refs.meta.conn.url = 'https://'+refs.meta.conn.hostName':'+refs.meta.conn.serverPort;
 //refs.meta.conn.peerKey = '6sdshp5kg3edbo6r';
 refs.meta.conn.peerKey = 'peerjs';
 refs.meta.conn.peerPath = '/peerjs';
